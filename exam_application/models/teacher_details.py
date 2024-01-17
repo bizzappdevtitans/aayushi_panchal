a473dcba34e367e8aa0bc12cdeab38f49308805b
bizzappdev <BAD@bizzappdev.com>
2024-01-17 19:22:09 +0530

    final done

--- exam_application/models/teacher_details.py
+++ exam_application/models/teacher_details.py
@@ -6,18 +6,18 @@ class TeacherDetails(models.Model):
     _description = "teacher information"
 
     # many2one filed
-    # student_id = fields.Many2one(comodel_name="student.details", string="student")
+    teacher_id = fields.Many2one(comodel_name="student.details", string="student")
 
     # defult value store in field
     exam_time = fields.Datetime(string="Exam time", defult=fields.Datetime.now)
     booking_date = fields.Date(
         string="Booking Exam Date", defult=fields.Date.context_today
     )
-    # gender = fields.Selection([("male", "Male"), ("female", "Female")],readonly=False)
+    gender = fields.Selection(related="teacher_id.gender",readonly=False)
     ref = fields.Char(string="Reference")
 
-    # @api.onchange("student_id")  # add decorater
-    # def onchange_teacher_id(
-    #     self,
-    # ):  # on change function when id selected automaticly ref change
-    #     self.ref = self.student_id
+    @api.onchange("teacher_id")  # add decorater
+    def onchange_teacher_id(
+        self,
+    ):  # on change function when id selected automaticly ref change
+        self.ref = self.teacher_id
